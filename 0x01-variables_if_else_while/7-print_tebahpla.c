c8a4d5eab9e37f7f08519b49097eab3337cc11d2
ardykay <adeyemikayous@gmail.com>
2022-10-16 13:58:12 -0700

    c var

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  * main - print the alphabet in reverse
+ * return Always 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)     /* betty style doc for function main goes there */
+{
+	char lc;
+
+	/* your code goes there */
+	for (lc = 'z'; lc >= 'z'; lc--)
+		putchar(lc);
+
+	putchar('\n');
+
+	return (0);
+}
