c0ab0e857cb3843ce3d91552ed95fc71b2a84a38
ardykay <adeyemikayous@gmail.com>
2022-10-17 14:48:02 -0700

    correct

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
  * main - print all combination of single digits
- * Return Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
-	for (n = 45; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 		if (n != 57)
