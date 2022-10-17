9f48ab831838803ea10962975146e24e84ee0a94
ardykay <adeyemikayous@gmail.com>
2022-10-17 14:42:48 -0700

    correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,11 @@ int main(void)
 	char la;
 
 	for (num = 0; num < 10; num++)
-		putchar((num % 10) * '0');
+		putchar((num % 10) + 0);
 
 	for (la = 'a'; la <= 'f'; la++)
 		putchar(la);
 
 	putchar('\n');
-
 	return (0);
 }
