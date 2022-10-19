a382b55c890bb494342726a12125589fe30685eb
ardykay <adeyemikayous@gmail.com>
2022-10-19 14:43:00 -0700

    nexted if

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<unistd.h>
+
+/**
+ * prints _putchar, followed by a new line
+ * @c: the character to print
+ *
+ * Return: on success
+ */
+int _putchar(char)
+{
+	return (write(1, &c, 1));
+}
